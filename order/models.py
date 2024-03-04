6700fe74d6a94d4d998db35ae766d607a38ffb02
b.amirov <amirovbobur52@gmail.com>
2024-03-04 16:22:17 +0500

    add cart

--- order/models.py
+++ order/models.py
@@ -5,20 +5,22 @@ from account.models import User
 from product.models import Product
 
 
-class Order(TimeStampedModel):
-    customer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='order')
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='order')
-    quantity = models.IntegerField(default=1)
+class OrderItem(TimeStampedModel):
+    customer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='orderitem')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='orderitem')
     total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.IntegerField(default=1)
+    ordered = models.BooleanField(default=False)
 
     def __str__(self):
         return f'{self.quantity} * {self.product.price} = {self.total_price}'
 
 
-class OrderItem(TimeStampedModel):
-    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='orderitem')
-    customer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='orderitem')
+class Order(TimeStampedModel):
+    customer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='order')
+    order = models.ManyToManyField(OrderItem)
     total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    ordered = models.BooleanField(default=False)
 
     def __str__(self):
         return f'{self.total_price}'
